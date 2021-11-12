695a43bcaee13c402416d3033ccc0b67c2dd716e
AMITJHA001 <amit.jhaa17@gmail.com>
2021-11-12 19:36:18 +0530

    Use Cases

--- Curringing/index.js
+++ Curringing/index.js
@@ -58,20 +58,98 @@ const { clear } = require("console");
 }
 
 
+
 //console.log(multiply(10)(10)(30));
 //Output:50
 
 or
 
 let multipleValue = multiply(10);
-multipleValue = multipleValue(10);
-multipleValue = multipleValue(30);
-console.log(multipleValue);
+multipleValue1 = multipleValue(10);
+multipleValue2 = multipleValue1(30);
+console.log(multipleValue2);
 //Output:50
 
 
+This is how multipleValue2 could perform the multiplication operation with variables defined in the already exit-ed functions. 
+Though the functions have long since returned and garbage collected from memory, yet its variables are somehow still kept "alive".
+
+
+function discount(n){
+    return (p) =>{
+        return p * n;
+    }
+}
+
+let tenPercentDiscount = discount(0.1);
+console.log(tenPercentDiscount(100));
+
+
+Example & useCase
+
+For example, we have a function to calculate the volume of a cylinder:
+
+function volume(l, w, h) {
+    return l * w * h;
+}
+
+It happens that all the cylinders in your warehouse🏠 are of height 100m. 
+You will see that you will repeatedly call this function with h as 100:
+
+
+volume(200,30,100) // 2003000l
+volume(32,45,100); //144000l
+volume(2322,232,100) // 53870400l
+
+function volume(h) {
+    return (w) => {
+        return (l) => {
+            return l * w * h
+        }
+    }
+}
+
+
+function volume(h) {
+    return (w,l) => {
+            return l * w * h
+    }
+}
+
+let calculateVolume = volume(100);
+let cylinderOne = calculateVolume(20,30);
+let cylinderTwo = calculateVolume(40,30);
+
+console.log(cylinderOne , cylinderTwo);
+//Output: 60000 120000
+
+
+Make Any Function as Curry function.
+
+function curry(fn, ...args) {
+    return (..._arg) => {
+        return fn(...args, ..._arg);
+    }
+}
+
+
+The idea behind currying is to take a function and derive a function that returns specialized function(s).
 
 */
+
+
+function curry(fn , ...arg ){
+    return (..._arg)=>{
+        return fn(...args, ..._arg);
+    }
+}
+
+curry();
+
+
+
+
+
 
 
 
